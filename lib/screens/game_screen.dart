02e13bee40108b60c307d6f460c9bc29a0abdc6e
Debjeet Das <debjeetald@gmail.com>
2022-03-04 15:40:43 +0530

    Fixed padding and added app icon

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -30,69 +30,61 @@ class GameScreen extends StatelessWidget {
         return Stack(
           children: [
             Scaffold(
-              appBar: AppBar(
-                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                foregroundColor: Colors.black,
-                elevation: 0.0,
-                centerTitle: true,
-                title: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: BlocBuilder<CandyCubit, CandyState>(
-                    buildWhen: (previous, current) =>
-                        previous.duration != current.duration,
-                    builder: (context, state) {
-                      return AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 500),
-                        transitionBuilder: (child, animation) {
-                          return ScaleTransition(
-                            scale: animation,
-                            child: child,
-                          );
-                        },
-                        child: Text(
-                          '${state.duration}',
-                          key: ValueKey<int>(state.duration),
-                          style: TextStyle(
-                            color:
-                                state.duration > 10 ? Colors.green : Colors.red,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                leading: BlocBuilder<CandyCubit, CandyState>(
-                  buildWhen: (previous, current) =>
-                      previous.candiesLeft != current.candiesLeft,
-                  builder: (context, state) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Text(
-                        'Left\n${state.candiesLeft}',
-                        textAlign: TextAlign.center,
-                      ),
-                    );
-                  },
-                ),
-                actions: [
-                  BlocBuilder<CandyCubit, CandyState>(
-                    buildWhen: (previous, current) =>
-                        previous.candiesSorted != current.candiesSorted,
-                    builder: (context, state) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(
-                          'Sorted\n${state.candiesSorted}',
-                          textAlign: TextAlign.center,
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
               body: SafeArea(
                 child: Column(
                   children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          BlocBuilder<CandyCubit, CandyState>(
+                            buildWhen: (previous, current) =>
+                                previous.candiesLeft != current.candiesLeft,
+                            builder: (context, state) {
+                              return Text(
+                                'Left\n${state.candiesLeft}',
+                                textAlign: TextAlign.center,
+                              );
+                            },
+                          ),
+                          BlocBuilder<CandyCubit, CandyState>(
+                            buildWhen: (previous, current) =>
+                                previous.duration != current.duration,
+                            builder: (context, state) {
+                              return AnimatedSwitcher(
+                                duration: const Duration(milliseconds: 500),
+                                transitionBuilder: (child, animation) {
+                                  return ScaleTransition(
+                                    scale: animation,
+                                    child: child,
+                                  );
+                                },
+                                child: Text(
+                                  '${state.duration}',
+                                  key: ValueKey<int>(state.duration),
+                                  style: TextStyle(
+                                    color: state.duration > 10
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          BlocBuilder<CandyCubit, CandyState>(
+                            buildWhen: (previous, current) =>
+                                previous.candiesSorted != current.candiesSorted,
+                            builder: (context, state) {
+                              return Text(
+                                'Sorted\n${state.candiesSorted}',
+                                textAlign: TextAlign.center,
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
                     SizedBox(
                       height: MediaQuery.of(context).size.height / 2,
                       child: const CandyArea(),
